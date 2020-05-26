98884c818f70325911975772dcce6415e7df84a0
makis <mazirat@gmail.com>
2020-05-26 22:31:38 +0300

    add css sass style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
   entry: {
     app: './src/index.js'
@@ -14,12 +14,38 @@ module.exports = {
       test: /\.js$/,
       loader: 'babel-loader',
       exclude: '/node_modules/'
-    }]
+    }, {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        },
+        {
+          loader: 'sass-loader',
+          options: { sourceMap: true }
+        }
+      ]
+    }, {
+      test: /\.css$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader"
+      ]
+    }
+  ]
   },
   devServer: {
     host: '0.0.0.0',
     port: 9000,
     disableHostCheck: true,
     overlay: true
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    })
+  ],
 }
