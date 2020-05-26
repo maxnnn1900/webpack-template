63711e4688dff3b8f6b98967423e1169afbf7577
makis <mazirat@gmail.com>
2020-05-26 21:53:30 +0300

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,13 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist'
   },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/'
+    }]
+  },
   devServer: {
     host: '0.0.0.0',
     port: 9000,
