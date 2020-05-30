bee74dddcde450361c08d4d51af11b4326a99b6d
maxnnn1900 <maxnnn1900@gmail.com>
2020-05-30 18:20:26 +0300

    clean build directory. show build progress.

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,9 +1,16 @@
+const webpack = require('webpack')
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
   mode: 'production',
-  plugins: []
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin({
+      verbose: true,
+    })
+  ]
 })
 
 module.exports = new Promise((reslove, reject) => {
